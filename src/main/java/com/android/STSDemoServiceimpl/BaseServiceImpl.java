8b982e3a6a42dbbd1dc24a6de6e16084bfacffa2
chengheng10 <chengheng10@163.com>
2018-06-17 09:47:22 +0800

    Repair

--- src/main/java/com/android/STSDemoServiceimpl/BaseServiceImpl.java
+++ src/main/java/com/android/STSDemoServiceimpl/BaseServiceImpl.java
@@ -13,46 +13,51 @@ import com.android.STSDemoService.BaseService;
 
 @Service("baseService")
 public class BaseServiceImpl implements BaseService {
-	
-	private AddMapper addMapper;
 
-	public AddMapper getAddMapper() {
-		return addMapper;
-	}
-	@Autowired
-	public void setAddMapper(AddMapper addMapper) {
-		this.addMapper = addMapper;
-	}
+    private AddMapper addMapper;
 
-	@Override
-	public String addInfo(Add addInfo) {
-		if (addMapper.insertSelective(addInfo) == 1) {
-			return "Add Success.";
-		}
-		return "添加失败";
-	}
-	@Override
-	public List<Add> getAll() {
-		return addMapper.getAll();
-	}
-	@Override
-	public String delete(String id) {
-		if (addMapper.deleteByPrimaryKey(id) == 1) {
-			return "删除成功";
-		}
-		return "删除失败";
-	}
-	@Override
-	public Add findById(String id) {
-		return addMapper.selectByPrimaryKey(id);
-	}
-	@Override
-	public String update(Add addInfo) {
-		if (addMapper.updateByPrimaryKeySelective(addInfo) == 1) {
-			return "更新成功";
-		}
-		return "更新失败";
-	}
-	
+    public AddMapper getAddMapper() {
+        return addMapper;
+    }
+
+    @Autowired
+    public void setAddMapper(AddMapper addMapper) {
+        this.addMapper = addMapper;
+    }
+
+    @Override
+    public String addInfo(Add addInfo) {
+        if (addMapper.insertSelective(addInfo) == 1) {
+            return "Add Success.";
+        }
+        return "添加失败";
+    }
+
+    @Override
+    public List<Add> getAll() {
+        return addMapper.getAll();
+    }
+
+    @Override
+    public String delete(String id) {
+        if (addMapper.deleteByPrimaryKey(id) == 1) {
+            return "删除成功";
+        }
+        return "删除失败";
+    }
+
+    @Override
+    public Add findById(String id) {
+        return addMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public String update(Add addInfo) {
+        if (addMapper.updateByPrimaryKeySelective(addInfo) == 1) {
+            return "更新成功";
+        }
+        return "更新失败";
+    }
+
 
 }
--- src/main/java/com/android/STSDemoUtil/FCM.java
+++ src/main/java/com/android/STSDemoUtil/FCM.java
@@ -13,157 +13,144 @@ import org.json.JSONObject;
 
 public class FCM {
 
-	final static private String FCM_URL = "https://fcm.googleapis.com/fcm/send";
+    final static private String FCM_URL = "https://fcm.googleapis.com/fcm/send";
 
-	/**
-	 * 
-	 * 
-	 * 
-	 * Method to send push notification to Android FireBased Cloud messaging
-	 * Server.
-	 * 
-	 * 
-	 * 
-	 * @param tokenId
-	 *            Generated and provided from Android Client Developer
-	 * 
-	 * @param server_key
-	 *            Key which is Generated in FCM Server
-	 * 
-	 * @param message
-	 *            which contains actual information.
-	 * 
-	 * 
-	 * 
-	 */
+    /**
+     * Method to send push notification to Android FireBased Cloud messaging
+     * Server.
+     *
+     * @param tokenId    Generated and provided from Android Client Developer
+     * @param server_key Key which is Generated in FCM Server
+     * @param message    which contains actual information.
+     */
 
-	public static String send_FCM_Notification(String tokenId, String server_key, String message,String typeID,String title) {
-		String result ="";
-		try {
+    public static String send_FCM_Notification(String tokenId, String server_key, String message, String typeID, String title) {
+        String result = "";
+        try {
 
-			// Create URL instance.
+            // Create URL instance.
 
-			URL url = new URL(FCM_URL);
+            URL url = new URL(FCM_URL);
 
-			// create connection.
+            // create connection.
 
-			HttpURLConnection conn;
+            HttpURLConnection conn;
 
-			conn = (HttpURLConnection) url.openConnection();
+            conn = (HttpURLConnection) url.openConnection();
 
-			conn.setUseCaches(false);
+            conn.setUseCaches(false);
 
-			conn.setDoInput(true);
+            conn.setDoInput(true);
 
-			conn.setDoOutput(true);
+            conn.setDoOutput(true);
 
-			// set method as POST or GET
+            // set method as POST or GET
 
-			conn.setRequestMethod("POST");
+            conn.setRequestMethod("POST");
 
-			// pass FCM server key
+            // pass FCM server key
 
-			conn.setRequestProperty("Authorization", "key=" + server_key);
+            conn.setRequestProperty("Authorization", "key=" + server_key);
 
-			// Specify Message Format
+            // Specify Message Format
 
-			conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Type", "application/json");
 
-			// Create JSON Object & pass value
+            // Create JSON Object & pass value
 
-			JSONObject infoJson = new JSONObject();
+            JSONObject infoJson = new JSONObject();
 
-			infoJson.put("title", title);
+            infoJson.put("title", title);
 
-			infoJson.put("body", message);
+            infoJson.put("body", message);
 
-			JSONObject typeInfo = new JSONObject();
-			typeInfo.put("type", typeID);
-			
-			JSONObject json = new JSONObject();
+            JSONObject typeInfo = new JSONObject();
+            typeInfo.put("type", typeID);
 
-			json.put("to", tokenId.trim());
+            JSONObject json = new JSONObject();
 
-			json.put("notification", infoJson);
-			json.put("data", typeInfo);
-			
-			
-			System.out.println("JsonObject:"+json.toString());
-			
-			OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            json.put("to", tokenId.trim());
 
-			wr.write(json.toString());
+            json.put("notification", infoJson);
+            json.put("data", typeInfo);
 
-			wr.flush();
 
-			int status = 0;
+            System.out.println("JsonObject:" + json.toString());
 
-			if (null != conn) {
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
 
-				status = conn.getResponseCode();
+            wr.write(json.toString());
 
-			}
+            wr.flush();
 
-			if (status != 0) {
+            int status = 0;
 
-				if (status == 200) {
+            if (null != conn) {
 
-					// SUCCESS message
+                status = conn.getResponseCode();
 
-					BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-					result = "200:"+ reader.readLine();
-					System.out.println("Android Notification Response : " + result);
+            }
 
-				} else if (status == 401) {
+            if (status != 0) {
 
-					// client side error
+                if (status == 200) {
 
-					System.out.println("Notification Response : TokenId : " + tokenId + " Error occurred :");
-					result = "401";
+                    // SUCCESS message
 
-				} else if (status == 501) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    result = "200:" + reader.readLine();
+                    System.out.println("Android Notification Response : " + result);
 
-					// server side error
+                } else if (status == 401) {
 
-					System.out.println("Notification Response : [ errorCode=ServerError ] TokenId : " + tokenId);
-					result = "501";
+                    // client side error
 
-				} else if (status == 503) {
+                    System.out.println("Notification Response : TokenId : " + tokenId + " Error occurred :");
+                    result = "401";
 
-					// server side error
+                } else if (status == 501) {
 
-					System.out.println("Notification Response : FCM Service is Unavailable  TokenId : " + tokenId);
-					result = "503";
-				}
+                    // server side error
 
-			}
+                    System.out.println("Notification Response : [ errorCode=ServerError ] TokenId : " + tokenId);
+                    result = "501";
 
-		} catch (MalformedURLException mlfexception) {
+                } else if (status == 503) {
 
-			// Prototcal Error
+                    // server side error
 
-			System.out.println("Error occurred while sending push Notification!.." + mlfexception.getMessage());
+                    System.out.println("Notification Response : FCM Service is Unavailable  TokenId : " + tokenId);
+                    result = "503";
+                }
 
-		} catch (IOException mlfexception) {
+            }
 
-			// URL problem
+        } catch (MalformedURLException mlfexception) {
 
-			System.out.println(
-					"Reading URL, Error occurred while sending push Notification!.." + mlfexception.getMessage());
-		} catch (JSONException jsonexception) {
+            // Prototcal Error
 
-			// Message format error
+            System.out.println("Error occurred while sending push Notification!.." + mlfexception.getMessage());
 
-			System.out.println(
-					"Message Format, Error occurred while sending push Notification!.." + jsonexception.getMessage());
-		} catch (Exception exception) {
+        } catch (IOException mlfexception) {
 
-			// General Error or exception.
+            // URL problem
 
-			System.out.println("Error occurred while sending push Notification!.." + exception.getMessage());
-		}
-		return result;
+            System.out.println(
+                    "Reading URL, Error occurred while sending push Notification!.." + mlfexception.getMessage());
+        } catch (JSONException jsonexception) {
 
-	}
+            // Message format error
+
+            System.out.println(
+                    "Message Format, Error occurred while sending push Notification!.." + jsonexception.getMessage());
+        } catch (Exception exception) {
+
+            // General Error or exception.
+
+            System.out.println("Error occurred while sending push Notification!.." + exception.getMessage());
+        }
+        return result;
+
+    }
 
 }
